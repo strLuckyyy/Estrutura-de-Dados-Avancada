72c0654f1436c00d0e07b1c8d4782cf1a9e3d915
Abrahão Francis <abrahaofcon@gmail.com>
2025-03-20 14:04:14 -0300

    cpp version

--- Aula 3/cpp-version.cpp
+++ Aula 3/cpp-version.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <string>
 
 using namespace std;
 
@@ -26,38 +27,72 @@ F(n) = 20n² + 2n + 5 == O(n²)
 Encontre a complexidade de tempo dos seguintes códigos:
 */
 
+void menu(double n);
 void a(double n);
 void b(double n);
 void c(double n);
 void d(double n);
 
 int main() {
-    double n;
-    cout << "Digite um número: ";
-    cin >> n;
+    double n = 100;
 
-    a(n);
-    b(n);
-    c(n);
-    d(n);
+    menu(n);
 
     return 0;
+}
+
+void menu(double n) {
+    cout << "Qual Algorítmo vc quer rodar?\n" << endl;
+    cout << "A) Complexidade O(log n)" << endl;
+    cout << "B) Complexidade O(log n)" << endl;
+    cout << "C) Complexidade O(n log n)" << endl;
+    cout << "D) Tempo infinito" << endl;
+    cout << "Digite a letra correspondente: ";
+    
+    string op;
+    getline(cin, op);
+
+    switch(op[0]) 
+    {
+        case 'A' | 'a':
+            a(n);
+            break;
+        case 'B' | 'b':
+            b(n);
+            break;
+        case 'C' | 'c':
+            c(n);
+            break;
+        case 'D' | 'd':
+            d(n);
+            break;
+        default:
+            cout << "Opção inválida" << endl;
+            return menu(n);
+    }
+
+    cout << "Deseja rodar outro algorítmo? (s/n)" << endl;
+    string resp;
+    getline(cin, resp);
+
+    if (resp[0] == 's' || resp[0] == 'S') {
+        return menu(n);
+    }
 }
 
 void a(double n) {
     int i = 1;
     while (i < n) {
         i *= 2;
-        cout << "a) complexidade: O(log n)" << endl;
-        cout << i << endl;
-    }    
+        cout << "a) I = " << i << "; Complexidade: O(log n)" << endl;
+    }
 }
 
 void b(double n) {
     int i = n;
 
     while (i > 0) {
-        cout << "b) complexidade: O(log n)" << endl;
+        cout << "b) I = " << i << "; Complexidade: O(log n)" << endl;
         i /= 2;
     }
 }
@@ -67,7 +102,7 @@ void c(double n) {
         int j = i;
         while (j < n) {
             j *= 2;
-            cout << "c) complexidade: O(n log n)" << endl;
+            cout << "c) I = " << i << "; J = " << j << "; Complexidade: O(n log n)" << endl;
         }
     }
 }
@@ -75,7 +110,7 @@ void c(double n) {
 void d(double n) {
     int i = 1;
     while (i < n) {
-        cout << "d) complexidade: tempo infinito" << endl;
+        cout << "d) I = " << i << "; Complexidade: tempo infinito" << endl;
         i = pow(i, 2);
     }
 }
